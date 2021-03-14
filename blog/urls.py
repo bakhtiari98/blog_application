9e1b5d382cecb0a68164a0a6ce7071d12cde966b
Mohammad Reza Bakhtiari <mrbakhtiary1377@gmail.com>
2021-03-14 20:40:50 +0330

    Define path for post_list and post_detail views

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,8 @@ app_name = 'blog'
 
 urlpatterns = [
     # post views
+    path('', views.post_list, name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+         views.post_detail,
+         name='post_detail'),
 ]
