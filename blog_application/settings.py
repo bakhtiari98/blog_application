70e7b110b56ef179ec099b7c64c77cf730fc8eee
Mohammad Reza Bakhtiari <mrbakhtiary1377@gmail.com>
2021-03-26 18:20:13 +0430

    add E-Mail settings

--- blog_application/settings.py
+++ blog_application/settings.py
@@ -121,3 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = local_settings.EMAIL_HOST_USER
+EMAIL_HOST_PASSWORD = local_settings.EMAIL_HOST_PASSWORD
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
